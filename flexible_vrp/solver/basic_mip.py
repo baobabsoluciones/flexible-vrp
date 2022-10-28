5c911da72609a69b36c2d24bbc47307f639b6c9c
Hugo Larzabal <hugo.larzabal@baobabsoluciones.es>
2022-10-28 14:09:24 +0200

    cambios en jsonschema, resolucion funciona.

--- flexible_vrp/solver/basic_mip.py
+++ flexible_vrp/solver/basic_mip.py
@@ -1,7 +1,7 @@
 # Class to solve the problem with a basic mip.
 
 from ..core import Experiment, Solution
-
+from .basic_mip_tools.create_model import create_model
 
 class BasicMip(Experiment):
     def __init__(self, instance, solution=None):
@@ -11,5 +11,6 @@ class BasicMip(Experiment):
         if not self.instance.to_dict():
             raise ValueError("The instance is empty")
         # Todo: replace this by the solve method
+        model = create_model()
         self.solution = Solution({"data": "This solver is not implemented yet"})
         return {}

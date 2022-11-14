3b538420339667554ca808922fc6904af1c68b78
marta sierra <marta.sierra.sanz00@gmail.com>
2022-11-14 14:41:30 +0100

    Modificaciones en los sets y parametros

--- flexible_vrp/solver/basic_mip_tools/create_model.py
+++ flexible_vrp/solver/basic_mip_tools/create_model.py
@@ -12,7 +12,8 @@ location = (r"C:\Users\maria\OneDrive - Universidad Politécnica de Madrid\datos
 valor = xlrd.open_workbook(location)
 
 hoja_param = valor.sheet_by_name("parameters")
-hoja_trp_duration = valor.sheet_by_name("trip_duration")
+hoja_warehouse = valor.sheet_by_name("warehouse")
+hoja_trip_duration = valor.sheet_by_name("trip_duration")
 hoja_inst1 = valor.sheet_by_name("comp_quantity_inst1")
 
 def create_model():
@@ -20,10 +21,26 @@ def create_model():
     model = AbstractModel()
 
     # Sets
-    model.sVehicles = Set([1,2,3,4,5,6,7,8])
-    model.sStops = Set()
-    model.sWarehouses = Set(["T1","T2","T3","T4","T5","F1","F2","F3"])
-    model.sCommodities = Set()
+    listaVeh=[]
+    model.sVehicles = Set(listaVeh)
+    for i in range(hoja_param.cell_value(2, 1)):
+        listaVeh.append(i+1)
+
+    listaStops=[]
+    model.sStops = Set(listaStops)
+
+    listaW = []
+    model.sWarehouses = Set(listaW)
+    for i in range(): #hasta que el excel detecte la casilla vacía
+        listaW.append(hoja_warehouse.cell_value(i,0)) #como decirle que es str
+
+    listaCom = []
+    model.sCommodities = Set(listaCom)
+    for i in range(): #hasta que el excel detecte la casilla vacía
+        listaCom.append([])
+        for j in range(3):
+            listaCom[i].append(hoja_inst1.cell_value(i,j)) #str
+
 
     # Derived sets
     model.sTripDurationDomain = Set(dimen=3)
@@ -41,23 +58,28 @@ def create_model():
     model.pUnloadTime = Param(hoja_param.cell_value(4, 1), mutable=True) # valor = 0.5 min
     model.pReqTimeLimit = Param(hoja_param.cell_value(5, 1), mutable=True) # valor = 8 hours = 480 min
     model.pOptTimeLimit = Param(hoja_param.cell_value(6, 1), mutable=True) # valor = 10 hours = 600 min
-    model.pBigM1 = Param(hoja_param.cell_value(7, 1), mutable=True) # valor = max Quantity = 143 pallets
+    model.pBigM1 = Param(hoja_param.cell_value(7, 1), mutable=True) # valor = max TripDuration = 35 min
     model.pBigM2 = Param(hoja_param.cell_value(8, 1), mutable=True) # valor = OptTimeLimit = 600 min
     model.pBigM3 = Param(hoja_param.cell_value(9, 1), mutable=True) # valor = OptTimeLimit-ReqTimeLimit = 120 min
 
-    model.pTripDuration = Param(model.sWarehouses * model.sWarehouses)
+    listaTrip = []
+    model.pTripDuration = Param(listaTrip)
+    for i in range(): #hasta que el excel detecte la casilla vacía
+        listaTrip.append([])
+        for j in range(3):
+            listaTrip[i].append(hoja_trip_duration.cell_value(i,j))  #leer string
 
-    model.pOrigin = Param(model.sCommodities[0], mutable=True)
-    model.pDestination = Param(model.sCommodities[1], mutable=True)
-    model.pQuantity = Param(model.sCommodities[2], mutable=True)
-    model.pReqCommodity = Param(model.sCommodities[3], mutable=True)
+    model.pOrigin =Param(, mutable=True)
+    model.pDestination = Param(, mutable=True)
+    model.pQuantity = Param(, mutable=True)
+    model.pReqCommodity = Param(, mutable=True)
 
 
 
     # Binary Variables
-    model.vAlpha = Var(model.sVehicles, model.sStops, model.sWarehouses) # domain=Binary)
-    model.vBeta = Var(model.sVehicles, model.sStops, model.sVehicles, model.sStops) # domain=Binary)
-    model.vBeta = Var(model.sVehicles, model.sStops, model.sVehicles, model.sStops) # domain=Binary)
+    model.vAlpha = Var(model.sVehicles, model.sStops, model.sWarehouses) #domain=Binary)
+    model.vBeta1 = Var(model.sVehicles, model.sStops, model.sVehicles, model.sStops) # domain=Binary)
+    model.vBeta2 = Var(model.sVehicles, model.sStops, model.sVehicles, model.sStops) # domain=Binary)
     model.vGamma = Var(model.sVehicles, model.sStops) # domain=Binary)
 
 

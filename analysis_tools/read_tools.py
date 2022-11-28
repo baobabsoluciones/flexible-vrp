66d66338c2eac935488b3bcdad5c296469e3b2b6
marta sierra <marta.sierra.sanz00@gmail.com>
2022-11-28 19:14:31 +0100

    añadimos los diccionarios a un solo diccionario

--- analysis_tools/read_tools.py
+++ analysis_tools/read_tools.py
@@ -6,15 +6,19 @@
 import pandas as pd
 datos = 'https://github.com/baobabsoluciones/flexible-vrp/blob/feature/base_mip/data/datos2.xlsx'
 
-sheet_param = pd.read_excel("datos1.xlsx", sheet_name ="parameters")
-sheet_warehouse = pd.read_excel("datos1.xlsx", sheet_name ="warehouse")
-sheet_trip_duration = pd.read_excel("datos1.xlsx", sheet_name ="trip_duration")
-sheet_inst1 = pd.read_excel("datos1.xlsx", sheet_name ="comp_quantity_inst1")
+sheet_param = pd.read_excel(datos, sheet_name ="parameters")
+sheet_warehouse = pd.read_excel(datos, sheet_name ="warehouse")
+sheet_trip_duration = pd.read_excel(datos, sheet_name ="trip_duration")
+sheet_inst1 = pd.read_excel(datos, sheet_name ="comp_quantity_inst1")
+
 #df.to_dict()
 dict_param = sheet_param.to_dict()
 dict_warehouse = sheet_warehouse.to_dict()
 dict_trip_duration = sheet_trip_duration.to_dict()
 dict_inst1= sheet_inst1.to_dict()
+
+dict1={'dict_param':dict_param,'dict_warehouse': dict_warehouse,'dict_trip_duration':dict_trip_duration,\
+    'dict_inst1':dict_inst1}
 
 # Sets
 listaVeh = []

ee7e41aa6eaf3e2de6d3ed48c395444449216585
marta sierra <marta.sierra.sanz00@gmail.com>
2022-11-28 19:24:42 +0100

    añadimos los diccionarios a un solo diccionario

--- flexible_vrp/solver/basic_mip.py
+++ flexible_vrp/solver/basic_mip.py
@@ -57,7 +57,46 @@ class BasicMip(Experiment):
         return dict(status=STATUS_TIME_LIMIT, status_sol=SOLUTION_STATUS_FEASIBLE)
         """
 
+        """
+        # Sets
+        listaVeh = []
+        Vehicles = listaVeh
+        for i in range(sheet_param.cell_value(2, 1)):
+            listaVeh.append(i + 1)
 
+        listaStops = []
+        Stops = listaStops
+
+        listaW = []
+        Wareahouses = listaW
+        for i in range(0, sheet_warehouse.nrows):
+            listaW.append(sheet_warehouse.cell_value(i, 0))  # como decirle que es str
+
+        listaCom = []
+        Commodities = listaCom
+        for i in range(0, sheet_inst1.nrows):
+            listaCom.append([])
+            for j in range(3):
+                listaCom[i].append(sheet_inst1.cell_value(i, j))  # str
+
+        # Parameters
+        VehCAP = sheet_param.cell_value(1, 1)  # valor = 22 pallets
+        FleetSize = sheet_param.cell_value(2, 1)  # valor= 8 vehicles
+        LoadTime = sheet_param.cell_value(3, 1)  # valor= 1 min
+        UnloadTime = sheet_param.cell_value(4, 1)  # valor = 0.5 min
+        ReqTimeLimit = sheet_param.cell_value(5, 1)  # valor = 8 hours = 480 min
+        OptTimeLimit = sheet_param.cell_value(6, 1)  # valor = 10 hours = 600 min
+        BigM1 = sheet_param.cell_value(7, 1)  # valor = max TripDuration = 35 min
+        BigM2 = sheet_param.cell_value(8, 1)  # valor = OptTimeLimit = 600 min
+        BigM3 = sheet_param.cell_value(9, 1)  # valor = OptTimeLimit-ReqTimeLimit = 120 min
+
+        listaTrip = []
+        TripDuration = listaTrip
+        for i in range(0, sheet_trip_duration.nrows):
+            listaTrip.append([])
+            for j in range(3):
+                listaTrip[i].append(sheet_trip_duration.cell_value(i, j))  # leer string
+        """
         model = create_model()
         self.solution = Solution({"data": "This solver is not implemented yet"})
         return {}

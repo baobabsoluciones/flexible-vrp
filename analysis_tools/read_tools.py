c325e6dc05f39a92451d372a74dc8ab4a9bb05b9
marta sierra <marta.sierra.sanz00@gmail.com>
2022-12-02 09:25:41 +0100

    corrección lectura de datos

--- analysis_tools/read_tools.py
+++ analysis_tools/read_tools.py
@@ -12,12 +12,20 @@ sheet_trip_duration = pd.read_excel(datos, sheet_name ="trip_duration")
 sheet_inst1 = pd.read_excel(datos, sheet_name ="comp_quantity_inst1")
 
 #df.to_dict()
-dict_param = sheet_param.to_dict()
+dict2_param = sheet_param.set_index('name')['value'].to_dict()
+df_param = sheet_param.to_records() #Crea una matriz con el índice, el nombre del parámetro y el valor
+print(sheet_param)
+print(" \n")
+print(dict2_param)
+print(" \n")
+print(df_param)
+
+
 dict_warehouse = sheet_warehouse.to_dict()
 dict_trip_duration = sheet_trip_duration.to_dict()
 dict_inst1= sheet_inst1.to_dict()
 
-dict1={'dict_param':dict_param,'dict_warehouse': dict_warehouse,'dict_trip_duration':dict_trip_duration,\
+dict1={'dict_param': dict_param,'dict_warehouse': dict_warehouse,'dict_trip_duration':dict_trip_duration,\
        'dict_inst1':dict_inst1}
 
 data = {

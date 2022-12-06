c820225e72a3d444326b79527d58a375d69eca94
marta sierra <marta.sierra.sanz00@gmail.com>
2022-12-06 18:30:46 +0100

    añadir nombre a las restricciones y terminar el diccionario completo de lectura de datos

--- flexible_vrp/solver/basic_mip.py
+++ flexible_vrp/solver/basic_mip.py
@@ -24,9 +24,11 @@ class BasicMip(Experiment):
         
         # Prepare data
         data = self.prepare_model_data(model)
-        vehicles = ["v{}".format(n) for n in range(1,NUM+1)]
+        vehicles = ["v{}".format(n) for n in range(1,fleet_size+1)]
+        stop
+        warehouse =["w{}".format(n)
         durations
-        # data= {None: {"sVehicles":{None:vehicles}, "param1":{None:1}, pTripDutation": {(1,1):3, (1,2):3}}}
+        # data= {None: {"sVehicles":{None:vehicles}, "param1":{None:1}, "pTripDutation": {(1,1):3, (1,2):3}}}
         model_instance = model.create_instance(data, report_timing=False)
         logfile = "./data/logfile.log"
         # Solve

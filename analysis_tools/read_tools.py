9e284d451673e9a1a87f3e18cf6afdb6f7cea0e8
Maria <maria.casanovac@alumnos.upm.es>
2022-12-06 17:30:11 +0100

    prueba2

--- analysis_tools/read_tools.py
+++ analysis_tools/read_tools.py
@@ -11,18 +11,24 @@ sheet_warehouse = pd.read_excel(datos, sheet_name ="warehouse")
 sheet_trip_duration = pd.read_excel(datos, sheet_name ="trip_duration")
 sheet_inst1 = pd.read_excel(datos, sheet_name ="comp_quantity_inst1")
 
-#df.to_dict()
-dict_param = sheet_param.to_dict()
+dict2_param = sheet_param.set_index('name')['value'].to_dict()
+df_param = sheet_param.to_records() #Crea una matriz con el índice, el nombre del parámetro y el valor
+print(sheet_param)
+print(" \n")
+print(dict2_param)
+print(" \n")
+print(df_param)
+print(" \n")
+
+
 dict_warehouse = sheet_warehouse.to_dict()
 dict_trip_duration = sheet_trip_duration.to_dict()
-dict_inst1= sheet_inst1.to_dict()
+dict_inst1 = sheet_inst1.to_dict()
 
-dict1={'dict_param':dict_param,'dict_warehouse': dict_warehouse,'dict_trip_duration':dict_trip_duration,\
-       'dict_inst1':dict_inst1}
-print(dict_param)
 print(dict_warehouse)
-print(dict1)
 
+dict1={'dict_param': dict2_param,'dict_warehouse': dict_warehouse,'dict_trip_duration':dict_trip_duration,\
+       'dict_inst1':dict_inst1}
 data = {
     "table1":[{"col1":1, "col2":3}, {"col1":2, "col2":3}, {"col1":3, "col2":3}],
     "table2":[{"col1":1, "col2":3}, {"col1":2, "col2":3}, {"col1":3, "col2":3}],

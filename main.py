c2a798d88fd50df7934e5c05667c41ae5a0525d8
Maria <maria.casanovac@alumnos.upm.es>
2022-12-15 18:12:00 +0100

    File_path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from analysis_tools.read_tools import read_input_data
 # Todo: create functions to read the config and data.
 config = {"solver": "basic_mip", "solver_name":"gurobi", "solver_config":{"TimeLimit":120, "gap":0.01}}
 # data = load_json("./data/simple_instance.json")
-file_path = "H:\Mi unidad\\academico\docencia\\tfx\en_curso\stn\codigo\\flexible-vrp\data\datos2.xlsx"
+file_path = "..\data\datos2.xlsx"
 data = read_input_data(file_path)
 
 # Solve the problem.

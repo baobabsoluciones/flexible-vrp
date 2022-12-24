1a21b0f7147f66199dcdc38a9f3d8668ebb7c5d6
marta sierra <marta.sierra.sanz00@gmail.com>
2022-12-24 10:22:29 +0100

    corrección errores

--- flexible_vrp/solver/basic_mip.py
+++ flexible_vrp/solver/basic_mip.py
@@ -14,7 +14,7 @@ class BasicMip(Experiment):
         # Calculating MILP parameters and sets
         """
         # Estimating bigMs
-        # Todo: estimate values as a funcion of instance parameters
+        # Todo: estimate values as a function of instance parameters
         bigM1 = max (data['pTripDuration'])
         bigM2 = data['pOptTimeLimit']
         bigM3 = (data['pOptTimeLimit'] - data['pReqTimeLimit'])
@@ -24,7 +24,7 @@ class BasicMip(Experiment):
         data = dict()
 
         # Adding the set for Warehouses
-        # Retrieving orings and destination
+        # Retrieving origins and destination
         origins = set([c["location1"] for c in self.instance.data["trip_durations"]])
         destinations = set(
             [c["location2"] for c in self.instance.data["trip_durations"]]
@@ -108,7 +108,7 @@ class BasicMip(Experiment):
             logfile=logfile,
         )
 
-        self.status = self.get_status(result)  # todo: create method
+        self.status = self.get_status(result)
         model_result = model_instance
         obj = model_instance.f_obj()
         print("Status: {} Objective value: {}".format(self.status, obj))
@@ -124,42 +124,40 @@ class BasicMip(Experiment):
 
         return 1  # dict(status=STATUS_TIME_LIMIT, status_sol=SOLUTION_STATUS_FEASIBLE)
 
-        """
-        Example of solve method:
-        
-        model = create_model()        
-
-        model_instance = model.create_instance(data, report_timing=False)
-        logfile = "./data/logfile.log"
-        # Solve
-        opt = self.set_solver(options)
-        try:
-            result = opt.solve(
-                model_instance,
-                tee=True,
-                warmstart=False,
-                logfile=logfile,
-            )
-        except ApplicationError:
-            message = "Solver error: a solver license may not be available to solve the model."
-            raise Exception(message)
-
-        self.status = get_status(result)
-        model_result = model_instance
-        obj = model_instance.f_obj()
-        print("Status: {} Objective value: {}".format(self.status, obj))
-
-        # Prepare solution
-        if is_feasible(self.status):
-            self.totals = self.get_total(model_result, result)
-            model_solution = self.get_model_solution(model_result)
-            self.solution = Solution(self.build_solution(model_solution))
-        else:
-            self.solution = self.get_empty_solution()
-            self.variables_to_excel(model_result)
-
-        return dict(status=STATUS_TIME_LIMIT, status_sol=SOLUTION_STATUS_FEASIBLE)
-        """
+        # Example of solve method:
+        #
+        # model = create_model()
+        #
+        # model_instance = model.create_instance(data, report_timing=False)
+        # logfile = "./data/logfile.log"
+        # # Solve
+        # opt = self.set_solver(options)
+        # try:
+        #     result = opt.solve(
+        #         model_instance,
+        #         tee=True,
+        #         warmstart=False,
+        #         logfile=logfile,
+        #     )
+        # except ApplicationError:
+        #     message = "Solver error: a solver license may not be available to solve the model."
+        #     raise Exception(message)
+        #
+        # self.status = get_status(result)
+        # model_result = model_instance
+        # obj = model_instance.f_obj()
+        # print("Status: {} Objective value: {}".format(self.status, obj))
+        #
+        # # Prepare solution
+        # if is_feasible(self.status):
+        #     self.totals = self.get_total(model_result, result)
+        #     model_solution = self.get_model_solution(model_result)
+        #     self.solution = Solution(self.build_solution(model_solution))
+        # else:
+        #     self.solution = self.get_empty_solution()
+        #     self.variables_to_excel(model_result)
+        #
+        # return dict(status=STATUS_TIME_LIMIT, status_sol=SOLUTION_STATUS_FEASIBLE)
 
         model = create_model()
         self.solution = Solution({"data": "This solver is not implemented yet"})

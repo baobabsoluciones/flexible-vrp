5faa269eb6566190393e43b0cd0a782afa60d683
marta sierra <marta.sierra.sanz00@gmail.com>
2022-12-24 10:43:05 +0100

    corrección errores

--- flexible_vrp/solver/basic_mip_tools/create_model.py
+++ flexible_vrp/solver/basic_mip_tools/create_model.py
@@ -1,4 +1,4 @@
-import pandas as pd
+
 from pyomo.environ import *
 
 # black won't reformat this file.
@@ -21,16 +21,16 @@ def create_model():
     model.c7_constraint_domain = Set(dimen=5)
 
     # Parameters
-    model.pVehCAP = Param(default=0, mutable=True) # valor = 22 pallets
-    model.pFleetSize = Param(default=0, mutable=True) # valor= 8 vehicles
-    model.pLoadTime = Param(default=0, mutable=True) # valor= 1 min
-    model.pUnloadTime = Param(default=0, mutable=True) # valor = 0.5 min
-    model.pReqTimeLimit = Param(default=0, mutable=True) # valor = 8 hours = 480 min
-    model.pOptTimeLimit = Param(default=0, mutable=True) # valor = 10 hours = 600 min
-    model.pBigM1 = Param(default=0, mutable=True) # valor = max TripDuration = 35 min
-    model.pBigM2 = Param(default=0, mutable=True) # valor = OptTimeLimit = 600 min
-    model.pBigM3 = Param(default=0, mutable=True) # valor = OptTimeLimit-ReqTimeLimit = 120 min
-    model.pTripDuration = Param(model.sWarehouses, model.sWarehouses, mutable=True) #esto es una lista/tabla
+    model.pVehCAP = Param(default=0, mutable=True)  # valor = 22 pallets
+    model.pFleetSize = Param(default=0, mutable=True)  # valor= 8 vehicles
+    model.pLoadTime = Param(default=0, mutable=True)  # valor= 1 min
+    model.pUnloadTime = Param(default=0, mutable=True)  # valor = 0.5 min
+    model.pReqTimeLimit = Param(default=0, mutable=True)  # valor = 8 hours = 480 min
+    model.pOptTimeLimit = Param(default=0, mutable=True)  # valor = 10 hours = 600 min
+    model.pBigM1 = Param(default=0, mutable=True)  # valor = max TripDuration = 35 min
+    model.pBigM2 = Param(default=0, mutable=True)  # valor = OptTimeLimit = 600 min
+    model.pBigM3 = Param(default=0, mutable=True)  # valor = OptTimeLimit-ReqTimeLimit = 120 min
+    model.pTripDuration = Param(model.sWarehouses, model.sWarehouses, mutable=True)  # esto es una lista/tabla
 
     # Binary Variables
     model.vAlpha = Var(model.sVehicles, model.sStops, model.sWarehouses, domain=Binary)
@@ -77,53 +77,55 @@ def create_model():
     def fc5_correct_unload(model, v, s, w):
         if model.sCommodities[1] == model.sWarehouses:
             return sum(model.vUnloadQuantity[v, s, c] for c in model.sCommodities) <= \
-            model.sCommodities[2] * model.vAlpha[v, s, w]
+                   model.sCommodities[2] * model.vAlpha[v, s, w]
         else:
             return Constraint.skip
 
     def fc6_max_load(model, v, s, w):
         if model.sCommodities[1] == model.sWarehouses:
             return sum(model.vLoadQuantity[v, s, c] for c in model.sCommodities) <= \
-            model.sCommodities[2] * model.vAlpha[v, s, w]  #model.sCommodities[2]
+                   model.sCommodities[2] * model.vAlpha[v, s, w]  # model.sCommodities[2]
         else:
             return Constraint.skip
 
     def fc7_trip_duration(model, v, s, w, w2):
-        if w!=w2:
+        if w != w2:
             s2 = s + 1
             return model.vTripDuration[v, s, s2] >= model.pTripDuration[w, w2] \
-            - model.pBigM1 * (2-model.vAlpha[v, s, w]-model.vAlpha[v, s2, w2])
+                - model.pBigM1 * (2 - model.vAlpha[v, s, w] - model.vAlpha[v, s2, w2])
         else:
             return Constraint.skip
 
     def fc8_unload_time(model, v, s):
         return model.vUnloadDuration[v, s] == sum(model.vUnloadQuantity[v, s, c] for c in model.sCommodities) \
-        * model.pUnloadTime
+               * model.pUnloadTime
 
     def fc9_load_time(model, v, s):
         return model.vLoadDuration[v, s] == sum(model.vLoadQuantity[v, s, c] for c in model.sCommodities) \
-        * model.pLoadTime
+               * model.pLoadTime
 
     def fc10_departure_time(model, v, s):
-        return model.vDepartureTime[v, s] == model.vArrivalTime[v, s] + model.vLoadDuration[v, s] + model.vUnloadDuration[v, s]
+        return model.vDepartureTime[v, s] == model.vArrivalTime[v, s] + model.vLoadDuration[v, s] \
+               + model.vUnloadDuration[v, s]
 
     def fc11_arrival_time(model, v, s):
-        s2=s+1
+        s2 = s+1
         return model.vArrivalTime[v, s2] == model.vDepartureTime[v, s] + model.vTripDuration[v, s, s2]
 
     def fc12_unload_time(model, v, s):
         return model.vUnloadTime[v, s] == model.vArrivalTime[v, s] + model.vUnloadDuration[v, s]
 
-    def fc13_simultaneidad_veh_1(model, v, s, v2, s2):
-        if v!=v2:
+    def fc13_simultaneity_veh_1(model, v, s, v2, s2, w):
+        if v != v2:
             return model.vBeta1[v, s, v2, s2] + model.vBeta2[v2, s2, v, s] >= model.vAlpha[v, s, w] \
-                   + model.vAlpha[v2, s2, w] -1
+                   + model.vAlpha[v2, s2, w] - 1
         else:
             return Constraint.skip
 
-    def fc14_simultaneidad_veh_2(model, v, s, v2, s2):
-        if v!= v2:
-            return model.vArrivalTime[v, s]>= model.vDepartureTime[v, s]- (1-model.vBeta1[v, s, v2, s2]) * model.pBigM2
+    def fc14_simultaneity_veh_2(model, v, s, v2, s2):
+        if v != v2:
+            return model.vArrivalTime[v, s] >= model.vDepartureTime[v, s] - (1-model.vBeta1[v, s, v2, s2]) \
+                   * model.pBigM2
         else:
             return Constraint.skip
 
@@ -131,7 +133,7 @@ def create_model():
         return model.vUnloadTime[v, s] <= model.pReqTimeLimit + model.vGamma[v, s] * model.pBigM3
 
     def fc16_time_limit_2(model, v, s, c):
-        if model.sCommodities[3]==1:
+        if model.sCommodities[3] == 1:
             return model.vLoadQuantity[v, s, c] <= (1-model.vGamma[v, s]) * model.pVehCAP
         else:
             return Constraint.skip
@@ -152,10 +154,10 @@ def create_model():
     model.c10_departure_time = Constraint(model.sVehicles, model.sStops, rule=fc10_departure_time)
     model.c11_arrival_time = Constraint(model.sVehicles, model.sStops, rule=fc11_arrival_time)
     model.c12_unload_time = Constraint(model.sVehicles, model.sStops, rule=fc12_unload_time)
-    model.c13_simultaneidad_veh_1 = Constraint(model.sVehicles, model.sStops, model.sVehicles, model.sStops,
-                                               rule=fc13_simultaneidad_veh_1)
-    model.c14_simultaneidad_veh_2 = Constraint(model.sVehicles, model.sStops, model.sVehicles, model.sStops,
-                                               rule=fc14_simultaneidad_veh_2)
+    model.c13_simultaneity_veh_1 = Constraint(model.sVehicles, model.sStops, model.sVehicles, model.sStops,
+                                              model.sWarehouses, rule=fc13_simultaneity_veh_1)
+    model.c14_simultaneity_veh_2 = Constraint(model.sVehicles, model.sStops, model.sVehicles, model.sStops,
+                                              rule=fc14_simultaneity_veh_2)
     model.c15_time_limit_1 = Constraint(model.sVehicles, model.sStops, rule=fc15_time_limit_1)
     model.c16_time_limit_2 = Constraint(model.sVehicles, model.sStops, model.sCommodities, rule=fc16_time_limit_2)
     model.c17_time_limit_3 = Constraint(model.sVehicles, model.sStops, rule=fc17_time_limit_3)

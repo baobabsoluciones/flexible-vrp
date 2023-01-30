98df2e9ba57b7578709d8fa2ae174715c7c0afcc
marta sierra <marta.sierra.sanz00@gmail.com>
2023-01-30 14:24:21 +0100

    eliminar warnings

--- flexible_vrp/solver/heuristic.py
+++ flexible_vrp/solver/heuristic.py
@@ -6,6 +6,7 @@ from flexible_vrp.solver.basic_mip_tools.create_model import create_model
 from cornflow_client.constants import PYOMO_STOP_MAPPING
 from heuristic_tools.create_solution import HeuristicSolution
 
+
 class Heuristic(Experiment):
     def __init__(self, instance, solution=None):
         super().__init__(instance, solution)

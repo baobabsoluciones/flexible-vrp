d083101214e1b95ec3a105a0217cf75526fe1b64
Maria <maria.casanovac@alumnos.upm.es>
2023-02-24 20:16:26 +0100

    clase heuristic

--- flexible_vrp/__init__.py
+++ flexible_vrp/__init__.py
@@ -6,7 +6,7 @@ from cornflow_client.core.tools import load_json
 
 from .core import Experiment, Instance, Solution
 from .solver.basic_mip import BasicMip
-
+from .solver.heuristic import Heuristic
 
 class FlexibleVRP(ApplicationCore):
     name = "flexible-vrp"
@@ -14,6 +14,7 @@ class FlexibleVRP(ApplicationCore):
     solution = Solution
     solvers = dict(
         basic_mip=BasicMip,
+        heuristic=Heuristic
     )
     schema = load_json(os.path.join(os.path.dirname(__file__), "./schemas/config.json"))
 

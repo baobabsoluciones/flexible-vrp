97bcfd67d50f9144061286720ee10d3f42c705e4
Maria <maria.casanovac@alumnos.upm.es>
2023-10-09 18:55:40 +0200

    solver: bassic mip

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ from analysis_tools.read_tools import read_input_data
 
 
 # Todo: create functions to read the config and data.
-# config = {
-#     "solver": "basic_mip",
-#     "solver_name": "gurobi",
-#     "solver_config": {"TimeLimit": 120, "gap": 0},
-# }
-
 config = {
-    "solver": "heuristic2",
-    "solver_config": {"TimeLimit": 120}
+    "solver": "basic_mip",
+    "solver_name": "gurobi",
+    "solver_config": {"TimeLimit": 600, "gap": 0},
 }
+
+# config = {
+#     "solver": "heuristic2",
+#     "solver_config": {"TimeLimit": 120}
+# }
 
 # data = load_json("./data/simple_instance.json")
 # file_path = "data/inst_1.xlsx"
-file_path = "data/inst_2.xlsx"
+# file_path = "data/inst_2.xlsx"
 # file_path = "data/inst_3.xlsx"
 # file_path = "data/inst_4.xlsx"
 # file_path = "data/inst_5.xlsx"
 # file_path = "data/inst_6.xlsx"
-# file_path = "data/dia_1.xlsx"
+file_path = "data/dia_1.xlsx"
 # file_path = "data/dia_2.xlsx"
 # file_path = "data/dia_3.xlsx"
 # file_path = "data/datos_min.xlsx"

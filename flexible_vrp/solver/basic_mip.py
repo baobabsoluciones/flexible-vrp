f6e7fad2e1e0f8c548176e8dd59a9740c96d2aef
marta sierra <marta.sierra.sanz00@gmail.com>
2024-07-09 19:49:23 +0200

    función para ejecutar varias instancias con varios solvers

--- flexible_vrp/solver/basic_mip.py
+++ flexible_vrp/solver/basic_mip.py
@@ -351,7 +351,7 @@ class BasicMip(Experiment):
         # name = 'datos_salida_' + inst_name
         # workbook.save(name)
 
-        workbook.save('datos_salida.xlsx')
+        workbook.save('data/data_salida/solucion.xlsx')
 
         data_solution_extra = TupList([[v, s, w, c[0], c[1], c[2], c[3],
                                         model_instance.vQuantityAtArrival[v, s, c].value,
@@ -391,37 +391,3 @@ class BasicMip(Experiment):
 
         return data_solution_extra
 
-        # Example of solve method:
-        #
-        # model = create_model()
-        #
-        # model_instance = model.create_instance(data, report_timing=False)
-        # logfile = "./data/logfile.log"
-        # # Solve
-        # opt = self.set_solver(options)
-        # try:
-        #     result = opt.solve(
-        #         model_instance,
-        #         tee=True,
-        #         warmstart=False,
-        #         logfile=logfile,
-        #     )
-        # except ApplicationError:
-        #     message = "Solver error: a solver license may not be available to solve the model."
-        #     raise Exception(message)
-        #
-        # self.status = get_status(result)
-        # model_result = model_instance
-        # obj = model_instance.f_obj()
-        # print("Status: {} Objective value: {}".format(self.status, obj))
-        #
-        # # Prepare solution
-        # if is_feasible(self.status):
-        #     self.totals = self.get_total(model_result, result)
-        #     model_solution = self.get_model_solution(model_result)
-        #     self.solution = Solution(self.build_solution(model_solution))
-        # else:
-        #     self.solution = self.get_empty_solution()
-        #     self.variables_to_excel(model_result)
-        #
-        # return dict(status=STATUS_TIME_LIMIT, status_sol=SOLUTION_STATUS_FEASIBLE)
